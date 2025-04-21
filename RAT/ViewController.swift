fc92d6e0d19c98e249448fccbe101f2390377f8e
Rio Simpson <rio.simpsonnyc@gmail.com>
2025-04-21 12:45:48 -0400

    corrected project template from MacOS to IOS

---
+++ RAT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RAT
+//
+//  Created by Rio Simpson on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
