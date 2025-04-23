70ab7373104d45e2a8a06619a6d1aa46f585ff3a
Rio Simpson <rio.simpsonnyc@gmail.com>
2025-04-22 23:35:10 -0400

    reusable module for different structs

--- RAT/Restaurant.swift
+++ RAT/Restaurant.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 struct Restaurant: Decodable {
-    // NYC Open Data fields
+    var camis: String?
     var dba: String?
     var building: String?
     var street: String?
@@ -18,9 +18,9 @@ struct Restaurant: Decodable {
     var grade: String?
     var inspection_date: String?
     var violation_description: String?
-    var grade_date: String?
     var critical_flag: String?
     var score: String?
+    var grade_date: String?
 }
 
 struct Violation {
@@ -28,4 +28,13 @@ struct Violation {
     let description: String
     let criticalFlag: String?
     let score: String?
+}
+
+struct FavoriteRestaurant: Codable {
+    let name: String
+    let grade: String?
+    let rating: Double?
+    let camis: String
+    let address: String
+    let placeID: String
 }
